f2f82bcd8d1da3c6416bbb83f8052cfef21a5de6
DannyvdZwart <32216@ma-web.nl>
2021-09-27 14:13:52 +0200

    Arduino code

---
+++ Tone/Tone.ino
@@ -0,0 +1,17 @@
+//A sketch to demonstrate the tone() function
+
+//Specify digital pin on the Arduino that the positive lead of piezo buzzer is attached.
+int piezoPin = 8;
+
+void setup() {
+
+}//close setup
+
+void loop() {
+
+  tone(piezoPin, 50000, 10000);
+
+  //tone(piezoPin, 1000, 500);
+  delay(1000);
+
+}
